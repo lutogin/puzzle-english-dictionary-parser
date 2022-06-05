649c21dc4320eb9a49dcf14e9a513bbd5dc739e4
aluto <aluto@softserveinc.com>
2022-06-05 20:47:40 +0300

    readme2

--- pe-parser/src/pe-client/pe-client.service.ts
+++ pe-parser/src/pe-client/pe-client.service.ts
@@ -36,8 +36,7 @@ export class PeClientService {
         Accept: '*/*',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         Connection: 'keep-alive',
-        Cookie:
-          'language_selected=ru; guest_id_int=16325749552941423; user_language=ru; undefined=undefined; amo-livechat-id=uO0n0f6MLIq7VZaZ1JQU6; _ga=GA1.2.696221981.1632814351; _ym_uid=1632814351368438692; _ym_d=1632814351; cto_bundle=46ixVl91SmNzY2ZRNDdzOGNMbHR6Z1dDaUlUSlRBYVllQ2NGMnBxQjAwOEVTZkVSSzduTnlYTks3QTF1VDc4QW5wSDRJVW93cGlBdE9ORmZMQkQwSWRqSGIwYXVwUDh6UWclMkZuc2M2ODRXbkVXVFA3eXYyJTJCTHVqaHMwWVpzTU9UWVJTOFpvQk1naUJuTzg5blBySG5BeTlERW5DYWRGdnAwdUlYN0V1eGtvS1V2THpzdUlUYnBEUHAwUXlpamJMaXZNMExjQmRieTAydXVWNVRZZjNsWU5HOVUyQSUzRCUzRA; amp_fdb811=WQrvO067T-rkz-zSmTIRyX.MTAwNTQ0MTc=..1fgs003ro.1fgs1hg0c.e.1.f; last_login=lutogin.v8%40gmail.com; tg_promotion-split-test=b; tg_mentors-banner-2022-02=b; wp_logged_in_cookie=Lutogin.v8%40gmail.com%7C1654435199%7CMJfSieUxh6OlmUhgEkNWsKZxeiQ8B9V0lI3LVRt0OdW%7C8bd7e8071f42b661d85fe34bbf4827b1c093099f9f79a2d69295236c9d7ab7c8; PHPSESSID=033db3b7eccdc29bf2538950a861f0c7; sort_dictionary=eyJ2aWV3IjoidGFibGUiLCJpdGVtIjoid29yZCIsInNvcnQiOjF9',
+        Cookie: this.configService.get<string>('COOKIE'),
       },
     };
   }
